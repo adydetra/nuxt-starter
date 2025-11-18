c55cd1c8eaee684e04ed019cd1d4fbe30d3e4622
adydetra <devdewa123.sp@gmail.com>
2025-11-18 22:41:15 +0700

    chore: cursor pointer in color mode button

--- app/components/Atoms/ColorMode.vue
+++ app/components/Atoms/ColorMode.vue
@@ -8,7 +8,7 @@ function setColorTheme(newTheme: Theme) {
 </script>
 
 <template>
-  <button aria-label="Color Mode" @click="setColorTheme($colorMode.preference === 'dark' ? 'light' : 'dark')">
+  <button aria-label="Color Mode" class="cursor-pointer" @click="setColorTheme($colorMode.preference === 'dark' ? 'light' : 'dark')">
     <Icon v-if="$colorMode.value === 'dark'" name="line-md:moon-filled-alt-loop" class="dark:text-white" :class="size" />
     <Icon v-else name="line-md:sunny-outline-loop" class="text-black" :class="size" />
   </button>
